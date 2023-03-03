7d130ec81320cd5bae2427352e17db2200b304df
Irfiyandaabidin <irfidupang@gmail.com>
2023-03-03 15:28:07 +0700

    first commit

---
+++ function.js
@@ -0,0 +1,202 @@
+let nama;
+let asal;
+let jurusan;
+let kampus;
+let semester;
+let umur;
+let namaDepan;
+let namaBelakang;
+let gender;
+let tahunLahir;
+let agama;
+let merkLaptop;
+let merkHp;
+let warnaHp;
+let namaMentor;
+let namaPIC;
+let bootcampTerbaik;
+let batch;
+let backend;
+let frontend;
+
+
+const setNama = (argNama) => {
+    nama = argNama
+}
+const getNama = () => {
+    return nama;
+}
+
+const setAsal = (argNama) => {
+    asal = argNama
+}
+const getAsal = () => {
+    return asal;
+}
+
+const setJurusan = (value) => {
+    jurusan = value
+}
+const getJurusan = () => {
+    return jurusan;
+}
+
+const setKampus = (value) => {
+    kampus = value
+}
+const getKampus = () => {
+    return kampus;
+}
+
+const setSemester = (value) => {
+    semester = value
+}
+const getSemester = () => {
+    return semester;
+}
+
+const setUmur = (value) => {
+    umur = value
+}
+const getUmur = () => {
+    return umur;
+}
+
+const setNamaDepan = (value) => {
+    namaDepan = value
+}
+const getNamaDepan = () => {
+    return namaDepan;
+}
+
+const setNamaBelakang = (value) => {
+    namaBelakang = value
+}
+const getNamaBelakang = () => {
+    return namaBelakang;
+}
+
+const setGender = (value) => {
+    gender = value
+}
+const getGender = () => {
+    return gender;
+}
+
+const setTahunLahir = (value) => {
+    tahunLahir = value
+}
+const getTahunLahir = () => {
+    return tahunLahir;
+}
+
+const setAgama = (value) => {
+    agama = value
+}
+const getAgama = () => {
+    return agama;
+}
+
+const setMerkLaptop = (value) => {
+    merkLaptop = value
+}
+const getMerkLaptop = () => {
+    return merkLaptop;
+}
+
+const setMerkHp = (value) => {
+    merkHp = value
+}
+const getMerkHp = () => {
+    return merkHp;
+}
+
+const setWarnaHp = (value) => {
+    warnaHp = value
+}
+const getWarnaHp = () => {
+    return warnaHp;
+}
+
+const setNamaMentor = (value) => {
+    namaMentor = value
+}
+const getNamaMentor = () => {
+    return namaMentor;
+}
+
+const setNamaPIC = (value) => {
+    namaPIC = value
+}
+const getNamaPIC = () => {
+    return namaPIC;
+}
+
+const setBootcampTerbaik = (value) => {
+    bootcampTerbaik = value
+}
+const getBootcampTerbaik = () => {
+    return bootcampTerbaik;
+}
+
+const setBatch = (value) => {
+    batch = value
+}
+const getBatch = () => {
+    return batch;
+}
+
+const setBackend = (value) => {
+    backend = value
+}
+const getBackend = () => {
+    return backend;
+}
+
+const setFrontend = (value) => {
+    frontend = value
+}
+const getFrontend = () => {
+    return frontend;
+}
+setNama("Irfi");
+setAsal("Pemalang")
+setJurusan("D3 Teknik Informatika")
+setKampus("AMIKOM Yogyakarta")
+setSemester(4)
+setUmur(19)
+setNamaDepan("Irfiyanda")
+setNamaBelakang("Abidin")
+setGender("L")
+setTahunLahir(2003)
+setAgama("Islam")
+setMerkLaptop("ASUS")
+setMerkHp("ASUS")
+setWarnaHp("Hitam")
+setNamaMentor("Ilham Fadhilah")
+setNamaPIC("Nadifah")
+setBootcampTerbaik("Productzilla")
+setBatch(4)
+setBackend(true)
+setFrontend(false)
+
+console.log(getNama())
+console.log(getAsal())
+console.log(getJurusan())
+console.log(getKampus())
+console.log(getSemester())
+console.log(getUmur())
+console.log(getNamaDepan())
+console.log(getNamaBelakang())
+console.log(getGender())
+console.log(getTahunLahir())
+console.log(getAgama())
+console.log(getMerkLaptop())
+console.log(getMerkHp())
+console.log(getWarnaHp())
+console.log(getNamaMentor())
+console.log(getNamaPIC())
+console.log(getBootcampTerbaik())
+console.log(getBatch())
+console.log(getBackend())
+console.log(getFrontend())
